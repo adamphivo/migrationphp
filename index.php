4e04bcd1ad766b1d0fc8b09648a17f1da9cda101
Adam Phi Vo <adamphivo@gmail.com>
2020-10-16 16:25:35 +0200

    Add autoloader

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 // Migration Script
 // Includes and auto-load
-include "PDO.php";
-include "Migration.php";
-include "Entity/Products.php";
-include "Entity/Productlines.php";
+spl_autoload_register(function ($class) {
+    include 'Entity/' . $class . '.php';
+    include $class . '.php';
+});
 
 // Main Script
 $migration = new Migration($pdo);
